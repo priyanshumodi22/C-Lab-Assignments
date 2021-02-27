87a2ac053662b0d4edb0f2c3e92fe977e5738d8e
priyanshumodi22 <priyanshumodi2001@gmail.com>
2021-02-28 00:17:38 +0530

    Lecture_11 Assignment

--- Lecture-11_C/Q5.c
+++ Lecture-11_C/Q5.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main()
 {
-     float i=1 , n , sum=0;
-     printf("Enter the value of n:");
-     scanf("%f",&n);
-     while(i<n)
-     {
-         sum=sum+(i/(i+2.0));            
-         i=i+4;
-     }
-     printf("1/3 + 5/7 + 9/11 +........+(n-2)/n\n");
+    int n;
+    float i , sum=0;
+    printf("Enter an integer (no. of terms): ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        sum=sum+(((4*i)-3)/((4*i)-1));
+    }
+     printf("\n1/3 + 5/7 + 9/11 +........+(n-2)/n\n");
      printf("Sum = %f",sum);
      
      return 0;
